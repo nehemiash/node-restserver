6237f7d9d0df675fa7f4fe69a4ada9fd8d47b583
Nemo <user@Mackie-Bockie.local>
2020-02-07 23:31:17 -0300

    Login Normal Seccion 10

--- server/config/config.js
+++ server/config/config.js
@@ -8,11 +8,17 @@ process.env.PORT = process.env.PORT || 3000;
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 // Base de Datos
-let urlDB; 
+let urlDB;
 
-if ( process.env.NODE_ENV === 'dev' ){
-    urlDB  = 'mongodb://localhost:27017/cafe'
-}else{
+// vencimiento del token
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+
+// SEED DE AUTENTICACION
+process.env.SEED = process.env.SEED || "este-es-el-seed-desarrollo";
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe'
+} else {
     urlDB = process.env.MONGO_URI;
 }
 
--- server/server.js
+++ server/server.js
@@ -13,17 +13,17 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-app.use(require('./routes/usuario'));
+//Configuracion Global de Rutas
+app.use(require('./routes/index'));
 
-mongoose.connect(process.env.URLDB,
-{ useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true },
-(err, res) => {
+mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true },
+    (err, res) => {
 
-    if (err) throw err;
+        if (err) throw err;
 
-    console.log('BD online');
+        console.log('BD online');
 
-});
+    });
 
 app.listen(process.env.PORT, () => {
     console.log("Escuchando en el puerto:", 3000);
